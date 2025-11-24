7ed4044f9656e5bd55f28e8e639ca39ea6b317ab
sammersaqa <sammersaqa@gmail.com>
2025-11-24 13:05:47 -0800

    adding cool wwe nicknames

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,74 +1,69 @@
-// Get elements
-let firstName = document.getElementById("firstName");
-let lastName = document.getElementById("lastName");
-let studentsBtn = document.getElementById("students");
-let instructorsBtn = document.getElementById("instructors");
-let historyList = document.getElementById("historyList");
+// Full roster with WWE nicknames
+const roster = {
+  students: [
+    { firstName: "Vanessa", lastName: "Acuna", wweName: "The Viper Queen" },
+    { firstName: "James", lastName: "Blackwell", wweName: "Blackout" },
+    { firstName: "Kellen", lastName: "Dixon", wweName: "The Iron Kid" },
+    { firstName: "Joseph", lastName: "Durate", wweName: "Durate the Dominator" },
+    { firstName: "Keante", lastName: "Epinger", wweName: "The Knockout King" },
+    { firstName: "Brandon", lastName: "Estrada", wweName: "Estrada Explosion" },
+    { firstName: "Nicolas", lastName: "Gonzaba", wweName: "The Gonzaba Grinder" },
+    { firstName: "Isaias", lastName: "Gonzales", wweName: "The Silent Storm" },
+    { firstName: "Eliasaf", lastName: "Gonzalez", wweName: "Ya Boy Eli – The Showstopper" },
+    { firstName: "Lucas", lastName: "Guptill", wweName: "Paradox Crusher" },
+    { firstName: "Daniel", lastName: "Herrera", wweName: "The Herrera Hammer" },
+    { firstName: "Arianna", lastName: "Huntley", wweName: "The Huntress" },
+    { firstName: "Xavier", lastName: "Jaques", wweName: "Xavy the X-Factor" },
+    { firstName: "Carlos", lastName: "Lopez", wweName: "Los Locos Lopez" },
+    { firstName: "Bryce", lastName: "Mack", wweName: "Big Mack Attack" },
+    { firstName: "Pallavi", lastName: "Pattanashetti", wweName: "The Panther" },
+    { firstName: "Jesus", lastName: "Ramirez", wweName: "El Ángel del Dolor" },
+    { firstName: "Jonathan", lastName: "Ramirez", wweName: "Nathan the Nightmare" },
+    { firstName: "Darcy", lastName: "Rose", wweName: "The Roseblade" },
+    { firstName: "Jesus", lastName: "Salgado", wweName: "Salgado the Savage" },
+    { firstName: "Sammer", lastName: "Saqa", wweName: "The Codebreaker" },
+    { firstName: "Gavin", lastName: "Tristan", wweName: "Tristan Titan" },
+    { firstName: "Brandon", lastName: "Van Horn", wweName: "Van Horn Havoc" }
+  ],
+  instructors: [
+    { firstName: "Ken", lastName: "Martinez", wweName: "Coach Crusher" },
+    { firstName: "Isaiah", lastName: "Furgeson", wweName: "The Enforcer" },
+    { firstName: "Jacob", lastName: "Dekok", wweName: "Jacoozzi the One Winged Angel" },
+    { firstName: "Jose", lastName: "Martinez", wweName: "El Jefe" }
+  ]
+};
 
+// DOM elements
+const firstNameEl = document.getElementById("firstName");
+const lastNameEl = document.getElementById("lastName");
+const historyList = document.getElementById("historyList");
+
+// History tracking
 let history = [];
 
-// Get random person from list
-function getRandomPerson(list) {
-  let randomIndex = Math.floor(Math.random() * list.length);
-  console.log("Random index:", randomIndex);
-  console.log("Random person:", list[randomIndex]);
-  return list[randomIndex];
-}
+// Utility: show random person from a group
+function showRandom(group) {
+  const people = roster[group];
+  const person = people[Math.floor(Math.random() * people.length)];
 
-// Show name on screen
-function showName(person) {
-  firstName.textContent = person.firstName;
-  lastName.textContent = person.lastName;
-  
-  // Log to console
-  console.log("--- Person Details ---");
-  console.log("Name:", person.firstName, person.lastName);
-  console.log("Email:", person.email);
-  console.log("Slack:", person.slackName);
-  console.log("---------------------");
-}
+  // Display with WWE nickname in the middle
+  firstNameEl.textContent = person.firstName;
+  lastNameEl.textContent = `"${person.wweName}" ${person.lastName}`;
 
-// Add to history list
-function addToHistory(person, type) {
-  history.unshift({ name: `${person.firstName} ${person.lastName} (${type})`, type: type });
+  // Track history (limit to last 5)
+  const fullDisplay = `${person.firstName} "${person.wweName}" ${person.lastName}`;
+  history.unshift(fullDisplay);
   if (history.length > 5) history.pop();
 
+  // Render history list
   historyList.innerHTML = "";
-  history.forEach((entry) => {
-    let li = document.createElement("li");
-    li.textContent = entry.name;
-    
-    // Add 'authority' class if it's The Authority
-    if (entry.type === "The Authority") {
-      li.classList.add("authority");
-    }
-    
+  history.forEach(name => {
+    const li = document.createElement("li");
+    li.textContent = name;
     historyList.appendChild(li);
   });
 }
 
-// Students button (CSA Superstars)
-studentsBtn.addEventListener("click", () => {
-  console.log("CSA Superstars button clicked!");
-  fetch("./data/data.json")
-    .then(response => response.json())
-    .then(data => {
-      console.log("Data loaded:", data);
-      let person = getRandomPerson(data.students);
-      showName(person);
-      addToHistory(person, "CSA Superstar");
-    });
-});
-
-// Instructors button (The Authority)
-instructorsBtn.addEventListener("click", () => {
-  console.log("The Authority button clicked!");
-  fetch("./data/data.json")
-    .then(response => response.json())
-    .then(data => {
-      console.log("Data loaded:", data);
-      let person = getRandomPerson(data.instructors);
-      showName(person);
-      addToHistory(person, "The Authority");
-    });
-});
+// Event listeners
+document.getElementById("students").addEventListener("click", () => showRandom("students"));
+document.getElementById("instructors").addEventListener("click", () => showRandom("instructors"));
